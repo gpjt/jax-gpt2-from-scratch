a15a6be91ba5cee1a440368c172918440fea45d1
Giles Thomas <giles@giles.net>
2026-06-02 18:57:45 +0100

    Use the JIT a bit better

--- test_smoke.py
+++ test_smoke.py
@@ -1,19 +1,28 @@
+from functools import partial
+
 import tiktoken
 
+import jax
 from flax import nnx
 from jax import numpy as jnp
 
 from gpt import GPTModel
 
 
+def generate_token(model, context):
+    logits = model(context)
+    last_token_logits = logits[:, -1, :]
+    predicted = jnp.argmax(last_token_logits, axis=-1)
+    expanded_predicted = jnp.expand_dims(predicted, axis=0)
+    context = jnp.append(context, expanded_predicted, axis=1)
+    return context
+
+
+@partial(jax.jit, static_argnums=(2,))
 def generate(model, seed_tokens, tokens_to_generate):
     context = jnp.array([seed_tokens])
     for ii in range(tokens_to_generate):
-        logits = model(context)
-        last_token_logits = logits[:, -1, :]
-        predicted = jnp.argmax(last_token_logits, axis=-1)
-        expanded_predicted = jnp.expand_dims(predicted, axis=0)
-        context = jnp.append(context, expanded_predicted, axis=1)
+        context = generate_token(model, context)
 
     return jnp.asarray(context.squeeze())
 
