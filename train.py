d8d5f04d6f08863318eba82f7f70b4cb712adf84
Giles Thomas <giles@giles.net>
2026-06-03 20:27:43 +0100

    Started building out the training script -- a runs dir

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
+import json
 from pathlib import Path
+
+import click
 
 from flax import nnx
 
@@ -6,18 +9,37 @@ from checkpointing import save_checkpoint
 from gpt import GPTModel
 
 
-def main():
+@click.command()
+@click.argument("run")
+def main(run):
+    run_dir = Path(__file__).resolve().parent / "runs" / run
+    if not run_dir.is_dir():
+        raise Exception(f"Could not find run dir {run_dir}")
+
+    model_conf_file = run_dir / "model.json"
+    if not model_conf_file.is_file():
+        raise Exception(f"Could not find model config in {model_conf_file}")
+    with open(model_conf_file, "r") as f:
+        model_conf = json.load(f)
+
+    train_conf_file = run_dir / "train.json"
+    if not train_conf_file.is_file():
+        raise Exception(f"Could not find train config in {train_conf_file}")
+    with open(train_conf_file, "r") as f:
+        train_conf = json.load(f)
+
     rngs = nnx.Rngs(42)
     model = GPTModel(
-        vocab_size=1024,
-        context_length=1024,
-        emb_dim=768,
-        n_heads=12, n_layers=12,
-        qkv_bias=False,
-        drop_rate=0.1,
+        vocab_size=model_conf["vocab_size"],
+        context_length=model_conf["context_length"],
+        emb_dim=model_conf["emb_dim"],
+        n_heads=model_conf["n_heads"],
+        n_layers=model_conf["n_layers"],
+        qkv_bias=model_conf["qkv_bias"],
+        drop_rate=train_conf["drop_rate"],
         rngs=rngs,
     )
-    save_checkpoint(Path("/tmp"), "checkpoint-test", model)
+    save_checkpoint(run_dir, "checkpoint-test", model)
 
 
 if __name__ == "__main__":
