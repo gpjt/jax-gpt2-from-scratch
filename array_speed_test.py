6e710c0a5a854378357ae410975d6dcba35498d3
Giles Thomas <giles@giles.net>
2026-06-15 19:08:21 +0100

    Simplification

--- array_speed_test.py
+++ array_speed_test.py
@@ -13,30 +13,33 @@ def main(commit):
 
     cpu0 = jax.devices("cpu")[0]
     with jax.default_device(cpu0):
-        base_array = jax.random.randint(
+        array = jax.random.randint(
             key,
-            (3_260_252_160,),
+            (530640, 6, 1024),
             0, 50_000,
             dtype=jax.numpy.uint16
         )
     if commit:
-        base_array = jax.device_put(base_array, cpu0)
+        array = jax.device_put(array, cpu0)
 
-    print(f"{datetime.now()}: {base_array.shape=}")
-    print(f"{datetime.now()}: {base_array.device=}")
-    print(f"{datetime.now()}: {base_array.committed=}")
-
-    reshaped = base_array.reshape(-1, 6, 1024)
-    print(f"{datetime.now()}: {reshaped.shape=}")
-    print(f"{datetime.now()}: {reshaped.device=}")
-    print(f"{datetime.now()}: {reshaped.committed=}")
+    print(f"{datetime.now()}: {array.shape=}")
+    print(f"{datetime.now()}: {array.device=}")
+    print(f"{datetime.now()}: {array.committed=}")
 
     start = time.time()
-    item = reshaped[0]
+    item = array[0]
     end = time.time()
 
-    print(f"{datetime.now()}: Getting item took {end - start}s")
+    print(f"{datetime.now()}: Getting first item took {end - start}s")
+    print(f"{datetime.now()}: {item.shape=}")
+    print(f"{datetime.now()}: {item.device=}")
+    print(f"{datetime.now()}: {item.committed=}")
 
+    start = time.time()
+    item = array[1]
+    end = time.time()
+
+    print(f"{datetime.now()}: Getting second item took {end - start}s")
     print(f"{datetime.now()}: {item.shape=}")
     print(f"{datetime.now()}: {item.device=}")
     print(f"{datetime.now()}: {item.committed=}")
