dd68b5b4e1c3d3bef4e22f679c96468a9e4b6463
Giles Thomas <giles@giles.net>
2026-06-20 02:18:54 +0100

    Added LayerNorm

--- gpt.py
+++ gpt.py
@@ -1,4 +1,21 @@
+import jax.numpy as jnp
 from flax import nnx
+
+
+class LayerNorm(nnx.Module):
+
+    def __init__(self, emb_dim):
+        self.scale = nnx.Param(jnp.ones((1, 1, emb_dim)))
+        self.bias = nnx.Param(jnp.zeros((1, 1, emb_dim)))
+
+    def __call__(self, xs):
+        means = xs.mean(axis=-1, keepdims=True)
+        stds = xs.std(axis=-1, keepdims=True)
+
+        normalized = (xs - means) / (stds + 1e-5)
+
+        scaled_and_biased = (normalized * self.scale) + self.bias
+        return scaled_and_biased
 
 
 class GPTModel(nnx.Module):
@@ -18,6 +35,8 @@ class GPTModel(nnx.Module):
             rngs=rngs,
         )
 
+        self.output_norm = LayerNorm(emb_dim)
+
         self.output_head = nnx.Linear(
             in_features=emb_dim,
             out_features=vocab_size,
@@ -29,4 +48,6 @@ class GPTModel(nnx.Module):
     def __call__(self, xs):
         input_embeddings = self.token_embedding(xs)
 
-        return self.output_head(input_embeddings)
+        normalised = self.output_norm(input_embeddings)
+
+        return self.output_head(normalised)
