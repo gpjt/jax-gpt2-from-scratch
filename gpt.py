9d0d87030705496156bc11b2e28a5d17762bbe53
Giles Thomas <giles@giles.net>
2026-06-23 02:48:51 +0100

    Single-head attention

--- gpt.py
+++ gpt.py
@@ -1,3 +1,4 @@
+import jax
 import jax.numpy as jnp
 from flax import nnx
 
@@ -8,6 +9,7 @@ class LayerNorm(nnx.Module):
         self.scale = nnx.Param(jnp.ones((1, 1, emb_dim)))
         self.bias = nnx.Param(jnp.zeros((1, 1, emb_dim)))
 
+
     def __call__(self, xs):
         means = xs.mean(axis=-1, keepdims=True)
         stds = xs.std(axis=-1, keepdims=True)
@@ -16,6 +18,49 @@ class LayerNorm(nnx.Module):
 
         scaled_and_biased = (normalized * self.scale) + self.bias
         return scaled_and_biased
+
+
+class Attention(nnx.Module):
+
+    def __init__(self, emb_dim, qkv_bias, rngs):
+        self.d_qk = emb_dim
+
+        self.W_q = nnx.Linear(emb_dim, self.d_qk, use_bias=qkv_bias, rngs=rngs)
+        self.W_k = nnx.Linear(emb_dim, self.d_qk, use_bias=qkv_bias, rngs=rngs)
+        self.W_v = nnx.Linear(emb_dim, self.d_qk, use_bias=qkv_bias, rngs=rngs)
+
+
+    def __call__(self, xs):
+        Q = self.W_q(xs)
+        K = self.W_k(xs)
+        V = self.W_v(xs)
+
+        omega = Q @ jnp.transpose(K, axes=(0, 2, 1))
+
+        omega /= jnp.sqrt(self.d_qk)
+
+        causal_mask = jnp.ones_like(omega, dtype=bool)
+        causal_mask = jnp.tril(causal_mask)
+
+        causal_omega = jnp.where(causal_mask, omega, -jnp.inf)
+
+        attention_weights = jax.nn.softmax(causal_omega, axis=-1)
+
+        return attention_weights @ V
+
+
+
+
+
+class TransformersLayer(nnx.Module):
+
+    def __init__(self, emb_dim, qkv_bias, rngs):
+        self.attention = Attention(emb_dim, qkv_bias, rngs)
+
+
+    def __call__(self, xs):
+        return self.attention(xs)
+
 
 
 class GPTModel(nnx.Module):
@@ -35,6 +80,8 @@ class GPTModel(nnx.Module):
             rngs=rngs,
         )
 
+        self.transformers_layer = TransformersLayer(emb_dim, qkv_bias, rngs)
+
         self.output_norm = LayerNorm(emb_dim)
 
         self.output_head = nnx.Linear(
@@ -48,6 +95,8 @@ class GPTModel(nnx.Module):
     def __call__(self, xs):
         input_embeddings = self.token_embedding(xs)
 
-        normalised = self.output_norm(input_embeddings)
+        transformed = self.transformers_layer(input_embeddings)
 
-        return self.output_head(normalised)
+        normalized = self.output_norm(transformed)
+
+        return self.output_head(normalized)
